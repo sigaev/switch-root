90ad78a631dfa5542e76df8c75eb582fd8216836
Dmitry Sigaev <www@sigaev.com>
2012-08-29 21:02:41 -0400

    Support AVX

--- x86gflops.c
+++ x86gflops.c
@@ -1,50 +1,104 @@
-#ifndef SSE2
-	#define MOVAP	"movaps"
-	#define ADDP	"addps"
-	#define MULP	"mulps"
-	#define PNAME	"sse"
-#else
-	#define MOVAP	"movapd"
-	#define ADDP	"addpd"
-	#define MULP	"mulpd"
-	#define PNAME	"sse2"
-#endif
+// PP + C99, 1 Dec 2005 MIT, 29 Aug 2012 Google
 
-#define id_string "x86" PNAME ".c (Thu Dec  1 16:54:58 2005) Dmitry Sigaev http://sigaev.com"
-// PP + C99, 1 Dec 2005 MIT
-
+#include <error.h>
+#include <immintrin.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 #include <sys/time.h>
 
-inline unsigned long long mytime(void)
-{
+static inline unsigned long long mytime() {
 	struct timeval tv;
 
 	return gettimeofday(&tv, NULL)
 		? 0ULL : 1000000ULL * tv.tv_sec + tv.tv_usec;
 }
 
-int main(int argc, char* argv[])
-{
-	static int n = 0;
-	static const float fp[4] __attribute__ ((aligned(16))) = { 0.0f };
-	unsigned long long t;
+static inline void usage() {
+	error(1, 0, "Usage: x86gflops sse|sse2|avx [n]");
+}
 
-	if(2 == argc)
-		n = atoi(argv[1]);
-	puts(id_string);
-	do
-	{
-		t = mytime();
+static float fp[128] __attribute__ ((aligned(32))) = { .0f };
+
+int main(int argc, char* argv[]) {
+	if(argc < 2 || argc > 3) {
+		usage();
+	}
+	enum {
+		SSE, SSE2, AVX
+	} mode;
+	if(!strcasecmp(argv[1], "sse")) {
+		mode = SSE;
+	} else if(!strcasecmp(argv[1], "sse2")) {
+		mode = SSE2;
+	} else if(!strcasecmp(argv[1], "avx")) {
+		mode = AVX;
+	} else {
+		usage();
+	}
+	int n = argc == 3 ? atoi(argv[2]) : 0;
+	do {
+		unsigned long long t = mytime();
+		if(mode == AVX) {
+			__m256 a0 = _mm256_load_ps(fp);
+			__m256 a1 = _mm256_load_ps(fp + 8);
+			__m256 a2 = _mm256_load_ps(fp + 16);
+			__m256 a3 = _mm256_load_ps(fp + 24);
+			__m256 a4 = _mm256_load_ps(fp + 32);
+			__m256 a5 = _mm256_load_ps(fp + 40);
+			__m256 a6 = _mm256_load_ps(fp + 48);
+			__m256 a7 = _mm256_load_ps(fp + 56);
+			__m256 a8 = _mm256_load_ps(fp + 64);
+			__m256 a9 = _mm256_load_ps(fp + 72);
+			__m256 aa = _mm256_load_ps(fp + 80);
+			__m256 ab = _mm256_load_ps(fp + 88);
+			__m256 ac = _mm256_load_ps(fp + 96);
+			__m256 ad = _mm256_load_ps(fp + 104);
+			__m256 ae = _mm256_load_ps(fp + 112);
+			__m256 af = _mm256_load_ps(fp + 120);
+			for(int i = 0; i < 500000000; ++i) {
+				a0 = _mm256_add_ps(a0, a0);
+				a1 = _mm256_mul_ps(a1, a1);
+				a2 = _mm256_add_ps(a2, a2);
+				a3 = _mm256_mul_ps(a3, a3);
+				a4 = _mm256_add_ps(a4, a4);
+				a5 = _mm256_mul_ps(a5, a5);
+				a6 = _mm256_add_ps(a6, a6);
+				a7 = _mm256_mul_ps(a7, a7);
+				a8 = _mm256_add_ps(a8, a8);
+				a9 = _mm256_mul_ps(a9, a9);
+				aa = _mm256_add_ps(aa, aa);
+				ab = _mm256_mul_ps(ab, ab);
+				ac = _mm256_add_ps(ac, ac);
+				ad = _mm256_mul_ps(ad, ad);
+				ae = _mm256_add_ps(ae, ae);
+				af = _mm256_mul_ps(af, af);
+			}
+			_mm256_store_ps(fp, _mm256_add_ps(
+				_mm256_add_ps(
+					_mm256_add_ps(_mm256_add_ps(a0, a1), _mm256_add_ps(a2, a3)),
+					_mm256_add_ps(_mm256_add_ps(a4, a5), _mm256_add_ps(a6, a7))),
+				_mm256_add_ps(
+					_mm256_add_ps(_mm256_add_ps(a8, a9), _mm256_add_ps(aa, ab)),
+					_mm256_add_ps(_mm256_add_ps(ac, ad), _mm256_add_ps(ae, af)))));
+		} else if(mode == SSE) {
+$$	foreach my $b(0 .. 1) {
+#ifndef SSE2
+	#define MOVAP	"movaps"
+	#define ADDP	"addps"
+	#define MULP	"mulps"
+#else
+	#define MOVAP	"movapd"
+	#define ADDP	"addpd"
+	#define MULP	"mulpd"
+#endif
 	__asm__ volatile(
 $$	local $" = "\n\t\t";
 $$	my $a = '$';
-		"pushal\n\t"
 		@{[map 'MOVAP "'."	(%0), %%xmm$_\n\t".'"', 0 .. 7]}
 		"movl	${a}4999999, %%eax\n\t"
 		".align	16\n\t"
-		"label:\n\t"
+		"label$b:\n\t"
 		"subl	${a}1, %%eax\n\t"
 #if 0
 /* looks like NetBurst is not very sensitive to the number
@@ -72,18 +126,21 @@ $$	foreach(0 .. 799) {
 		MULP "	%%xmm@{[(2*$_ + 1) & 7]}, %%xmm@{[(2*$_ + 1) & 7]}\n\t"
 $$	}
 #endif
-		"jnc	label\n\t"
-		"popal\n\t"
+		"jnc	label$b\n\t"
 		:
 		: "r" (fp)
 		: "xmm0", "xmm1", "xmm2", "xmm3", "xmm4", "xmm5", "xmm6", "xmm7"
 	);
-		printf("%.3f Gflops\n",
-#ifndef SSE2
-			32e6
-#else
-			16e6
-#endif
+$$	if($b == 0) {
+		} else {
+#define SSE2
+#undef ADDP
+#undef MULP
+#undef MOVAP
+$$	}
+$$	}
+		}
+		printf("%.3f Gflops\n", (mode == AVX ? 64e6 : (mode == SSE ? 32e6 : 16e6))
 			/ (double)(mytime() - t));
 	}
 	while(0 < --n);
