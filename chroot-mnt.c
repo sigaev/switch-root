d6067e5ba02893cd425e7f40f6ce3513faa3f39f
Dmitry Sigaev <www@sigaev.com>
2012-09-07 01:42:15 -0400

    Add chroot-undo

--- chroot-mnt.c
+++ chroot-mnt.c
@@ -1,10 +1,28 @@
+#include <dirent.h>
 #include <errno.h>
 #include <error.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
 #include <unistd.h>
 
+static const char root[] = "/mnt";
+
+static int chroot_plus(bool undo) {
+	DIR* dir = opendir(".");
+	if(!dir) return errno;
+	if(!chdir("/"))
+	if(!chroot(root))
+	if(!undo || !chroot("../../../.."))
+	if(!fchdir(dirfd(dir)))
+		return closedir(dir) ? errno : 0;
+	const int err = errno;
+	closedir(dir);
+	return err;
+}
+
 int main(int argc, char* argv[]) {
-	static const char root[] = "/mnt";
-	const int err = chroot(root) ? errno : 0;
+	const int err = chroot_plus(strstr(argv[0], "undo"));
 	if(setuid(getuid())) return 1;  // Do not report errors while root.
 	if(err) {
 		error(1, err, "chroot(%s)", root);
